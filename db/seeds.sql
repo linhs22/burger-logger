2f5835e5bf61ad5227b3d96c464575eb33d9bd1e
Henry Lin <henrylin58@gmail.com>
2020-01-17 23:47:58 -0800

    added basic functionality

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('South of the Border Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
