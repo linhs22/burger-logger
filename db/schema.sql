ebe33487fb6a68e17b2b9816cf239f7264b71dd9
Henry Lin <henrylin58@gmail.com>
2020-01-21 09:39:39 -0800

    adding full functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
 ### Schema
 
+DROP DATABASE IF EXISTS burgers_DB;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Hamburger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('South of the Border Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
-INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('South of the Border Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Mushroom Swiss Burger', true);
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
